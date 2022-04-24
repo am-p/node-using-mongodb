62e39d5260183c677196a8bdbd44594de4b75aa6
Ariel Pardo <adoorno@gmail.com>
2022-04-24 20:17:57 -0300

    Completed module

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import mongoose from 'mongoose';
 const app = express();
 const PORT = 4000;
 
+// mongoose connection 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
@@ -16,9 +23,3 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
-
-mongoose.connect('mongodb://localhost:27017/productsdb');
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.Promise = global.Promise;
-
